4a3aa123b482bd85e49fef43c4fa7cdf1f19d798
petermucsi <petermucsi@10.0.1.200>
2009-09-29 22:16:12 +0100

    Form submit and handler

--- clojwebapp1/src/com/peti/clojwebapp1.clj
+++ clojwebapp1/src/com/peti/clojwebapp1.clj
@@ -6,13 +6,24 @@
     (use compojure)
     (use clojure.core))
 
-(defn buildform [params]
-  (str params))
+(defn blog-entry-form [params]
+  (html
+    (form-to [:post "/blog-entry-form-handler"]
+      (text-area {:rows 20 :cols 73} "area1")
+      [:br]
+      (submit-button "Save"))))
+
+(defn blog-entry-success [params]
+  (html
+    (text-area "Entry Saved")))
+
+(defn blog-entry-form-handler [params]
+  (if (nil? (get params :area1))
+        (blog-entry-form params)
+        (blog-entry-success params)))
 
 (defroutes petiapp
-  (GET "/hello" (buildform params))
+  (GET "/blog-entry-form" (blog-entry-form params))
+  (POST "/blog-entry-form-handler" (blog-entry-form-handler params))
   (ANY "*" (page-not-found)))
-
-(defn -main []
-  (println "loszar"))
 
